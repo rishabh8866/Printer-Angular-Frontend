8c1baaac33bfc1af3adef19f63671746600ae549
Rubin Gajera <rgajera@vizetto.com>
2019-05-30 16:57:09 -0400

    Response is coming in Json format on frontend

--- brother-printer/src/app/scan-to-email/scan-to-email.component.ts
+++ brother-printer/src/app/scan-to-email/scan-to-email.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {HttpService} from "../service/http.service";
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-scan-to-email',
@@ -15,18 +16,23 @@ public scanToEmailData = {
   Resolution: "Normal",
   FileType: "PDF"
 }
-public scanTrayList = ['ADF'];
-public colorModeList = ['Color', 'Gray', 'Mono'];
-public resolutionList = ['Normal', 'Low', 'High'];
-public fileTypeList = ['PDF', 'JPEG'];
+public scanTrayList = ['FB', 'ADF', 'Auto'];
+public colorModeList = ['Color', 'Gray', 'Mono', 'Auto'];
+public resolutionList = ['Normal', 'Low', 'High', '600', '400', '300', '200', '150', '100', '200x100', 'Auto'];
+public fileTypeList = ['PDF', 'JPEG', 'HighCompressedPDF', 'PDFA', 'SecurePDF', 'SignedPDF', 'XPS', 'TIFF'];
 public isMultiple = false;
 public canMultiSelect = false;
 public multiEmail = [];
 public email = '';
 public password = '';
 public noOfEmial = 1;
-  constructor(private http: HttpService) { }
-
+public result = ['', '', '', ''];
+constructor(private http: HttpService, private socket: Socket) {
+  this.socket.on('getXml', (data) => {
+    console.log(data);
+    this.result[data.s] = data.data;
+  });
+}
   send(multiEmail) {
     if(!multiEmail) {
       this.http.post('/file/commandxml/add', {"ScanToEmail": this.scanToEmailData}).subscribe(() => {
