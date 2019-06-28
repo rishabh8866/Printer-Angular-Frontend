d1bee542692a4b7023175d2ab3c3851be01767d4
rishabh <rishabhchopra.dcs16@vnsgu.ac.in>
2019-06-28 15:03:33 +0530

    Added more fields in scan to email

--- brother-printer/src/app/constant.ts
+++ brother-printer/src/app/constant.ts
@@ -1,3 +1,17 @@
 export const ColorModeList = ['Color', 'Gray', 'Mono', 'Auto'];
-export const FeedTrayList = ['Color', 'Gray', 'Mono', 'Auto'];
-export const PageSizeList = ['letter', 'Auto', 'A4', 'A3'];
+export const FeedTrayList = ['Auto', 'MP', 'T1', 'T2', 'T3', 'T4', 'T5'];
+export const PageSizeList = ['Letter', 'Auto', 'A4', 'A3', 'A5', 'A5L', 'A6', 'B5', 'Legal', 'IndiaLegal',
+'MexicoLegal', 'Executive', 'Hagaki', 'Folio', 'Com10', 'Monarch', 'C5', 'DL'];
+export const CopyQuality = ['Best', 'Normal', 'Fast'];
+export const Layout = ['Normal', '2in1', '4in1'];
+export const LayoutOrientation = ['Portrait', 'Landscape'];
+export const DuplexCopyPageLayout = ['LongToLongEdge', 'LongToShortEdge', 'ShortToLongEdge', 'ShortToShortEdge'];
+export const FaxQuality = ['Standard', 'Fine', 'SuperFine', 'Photo'];
+export const PrintQuality = ['Normal', 'Fine'];
+export const Resolution = ['High', 'Normal', 'Low', '600', '400', '200' ,'150', '100', '200x100', 'Auto'];
+export const FileType = ['TIFF', 'JPEG', 'PDF', 'XPS', 'SignedPDF', 'SecurePDF' ,'PDFA', 'HighCompressedPDF'];
+export const DocSize = ['Letter', 'Auto', 'A4', 'A3', 'A5', 'A5L', 'A6', 'B5', 'Legal', 'IndiaLegal',
+  'MexicoLegal', 'Executive', 'Hagaki', 'Folio', 'Com10', 'Monarch', 'BusinessCard', 'LongPaper', 'Photo2L',
+'PhotoL', '4times6'];
+export const PaperType = ['Plain', 'Thin', 'Recycled', 'Thick', 'Thicker', 'Bond', 'Label',
+'Envelope', 'EnvelopeThin', 'EnvelopeThick', 'Letterhead', 'Colored'];
